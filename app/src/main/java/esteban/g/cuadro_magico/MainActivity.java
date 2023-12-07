e0ec20b564dacc25a4cc046ff4afa7473763fe81
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2023-12-07 16:07:40 -0500

    v0

--- app/src/main/java/esteban/g/cuadro_magico/MainActivity.java
+++ app/src/main/java/esteban/g/cuadro_magico/MainActivity.java
@@ -2,11 +2,18 @@ package esteban.g.cuadro_magico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,8 +42,12 @@ public class MainActivity extends AppCompatActivity {
         // Limpiar el GridLayout antes de mostrar el nuevo cuadrado mágico
         gridLayout.removeAllViews();
 
+        int[] rowSums = new int[magicSquare.length];
+        int[] colSums = new int[magicSquare[0].length];
         // Mostrar los números en el GridLayout
+
         for (int i = 0; i < magicSquare.length; i++) {
+            int rowSum = 0;
             for (int j = 0; j < magicSquare[i].length; j++) {
                 TextView textView = new TextView(this);
                 textView.setText(String.valueOf(magicSquare[i][j]));
@@ -49,22 +60,151 @@ public class MainActivity extends AppCompatActivity {
                 textView.setLayoutParams(params);
 
                 gridLayout.addView(textView);
+
+                rowSum += magicSquare[i][j];
+                colSums[j] += magicSquare[i][j];
             }
+            rowSums[i] = rowSum;
         }
+        // mostrar resultados
+        for (int i = 0; i < rowSums.length; i++) {
+            TextView rowSumTextView = new TextView(this);
+            rowSumTextView.setText("= "+ rowSums[i]);
+            rowSumTextView.setTextSize(15);
+            rowSumTextView.setPadding(8, 8, 8, 8);
+
+            GridLayout.LayoutParams rowParams = new GridLayout.LayoutParams();
+            rowParams.rowSpec = GridLayout.spec(i);
+            rowParams.columnSpec = GridLayout.spec(4); // Columna adicional para las sumas de filas
+            rowSumTextView.setLayoutParams(rowParams);
+
+            gridLayout.addView(rowSumTextView);
+        }
+
+        for (int i = 0; i < colSums.length; i++) {
+            TextView colSumTextView = new TextView(this);
+            colSumTextView.setText("= "+ String.valueOf(colSums[i]));
+            colSumTextView.setTextSize(15);
+            colSumTextView.setPadding(8, 8, 8, 8);
+
+            GridLayout.LayoutParams colParams = new GridLayout.LayoutParams();
+            colParams.rowSpec = GridLayout.spec(4); // Fila adicional para las sumas de columnas
+            colParams.columnSpec = GridLayout.spec(i);
+            colSumTextView.setLayoutParams(colParams);
+
+            gridLayout.addView(colSumTextView);
+        }
+
+        int diagonalSum1 = 0;
+        for (int i = 0; i < magicSquare.length; i++) {
+            diagonalSum1 += magicSquare[i][i];
+        }
+
+// Calcular la suma de la diagonal secundaria
+        int diagonalSum2 = 0;
+        for (int i = 0; i < magicSquare.length; i++) {
+            diagonalSum2 += magicSquare[i][magicSquare.length - 1 - i];
+        }
+
+// Mostrar la suma de la diagonal principal
+        TextView diagonalSum1TextView = new TextView(this);
+        diagonalSum1TextView.setId(View.generateViewId()); // Genera un ID único para el TextView
+        diagonalSum1TextView.setText("Suma diagonal 1: " + diagonalSum1);
+        diagonalSum1TextView.setTextSize(15);
+        diagonalSum1TextView.setPadding(8, 8, 8, 8);
+
+        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        params1.addRule(RelativeLayout.BELOW, R.id.gridLayout); // ID del GridView o elemento debajo del cual se quiere colocar
+        params1.addRule(RelativeLayout.ALIGN_PARENT_START);
+        diagonalSum1TextView.setLayoutParams(params1);
+
+// Añadir el TextView al layout principal de tu actividad (fuera del GridLayout)
+        RelativeLayout mainLayout = findViewById(R.id.mainLayout); // Cambia esto por el ID de tu layout principal
+        mainLayout.addView(diagonalSum1TextView);
+
+// Mostrar la suma de la diagonal secundaria
+        TextView diagonalSum2TextView = new TextView(this);
+        diagonalSum2TextView.setId(View.generateViewId()); // Genera un ID único para el TextView
+        diagonalSum2TextView.setText("Suma diagonal 2: " + diagonalSum2);
+        diagonalSum2TextView.setTextSize(15);
+        diagonalSum2TextView.setPadding(8, 8, 8, 8);
+
+        RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        params2.addRule(RelativeLayout.BELOW, R.id.gridLayout); // ID del GridView o elemento debajo del cual se quiere colocar
+        params2.addRule(RelativeLayout.END_OF, diagonalSum1TextView.getId());
+        params2.addRule(RelativeLayout.ALIGN_PARENT_END);
+        diagonalSum2TextView.setLayoutParams(params2);
+
+// Añadir el TextView al layout principal de tu actividad (fuera del GridLayout)
+        mainLayout.addView(diagonalSum2TextView);
     }
 
     private int[][] generateMagicSquare() {
         int[][] square = new int[4][4];
 
-        // Llenar la matriz con números aleatorios de 4 dígitos hechos de 5 y/o 7
+        for (int i = 0; i < square.length; i++) {
+            Arrays.fill(square[i], 0);
+        }
+
+        ArrayList<Integer> remainingNumbers = new ArrayList<>();
         for (int i = 0; i < square.length; i++) {
             for (int j = 0; j < square[i].length; j++) {
                 square[i][j] = generateRandomNumber();
+                if (i == 0){
+                    remainingNumbers.add(square[i][j]);
+                }
+            }
+        }
+
+        Random random = new Random();
+        while (!checkMagicSquare(square, remainingNumbers.stream().mapToInt(Integer::intValue).sum())) {
+            for (int i = 1; i < square.length; i++) {
+                ArrayList<Integer> aux = new ArrayList<>(remainingNumbers);
+                for (int j = 0; j < square[i].length; j++) {
+                    int index = 1;
+                    if (aux.size() != 0){
+                        index = random.nextInt(aux.size());
+                    }
+                    square[i][j] = aux.get(index);
+                    //square[j][i] = square[i][j];
+                    aux.remove(index);
+                }
             }
         }
 
         return square;
     }
+
+    private boolean checkMagicSquare(int[][] square, int magicSum) {
+        // Verifica la suma de todas las filas, columnas y diagonales principales
+        for (int i = 0; i < square.length; i++) {
+            int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < square[i].length; j++) {
+                rowSum += square[i][j];
+                colSum += square[j][i];
+            }
+            if (rowSum != magicSum || colSum != magicSum) {
+                return false;
+            }
+        }
+
+        int diag1Sum = 0;
+        int diag2Sum = 0;
+        for (int i = 0; i < square.length; i++) {
+            diag1Sum += square[i][i];
+            diag2Sum += square[i][square.length - i - 1];
+        }
+        return diag1Sum == magicSum && diag2Sum == magicSum;
+    }
+
+
 
     private int generateRandomNumber() {
         int number;
